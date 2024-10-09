0de23b848d32d874ae87a071fdcbe923e3524de8
Mihai Chirculescu <mihai.chirculescu@yahoo.com>
2024-10-09 23:14:55 +0300

    Update backtrack_sampler_provider.py

--- provider/backtrack_sampler_provider.py
+++ provider/backtrack_sampler_provider.py
@@ -12,7 +12,7 @@ class BacktrackSamplerProvider(ABC):
         pass
 
     @abstractmethod
-    def generate(self, input_ids: torch.Tensor) -> torch.Tensor:
+    def generate(self, input_ids: torch.Tensor, *args, **kwargs) -> torch.Tensor:
         pass
 
     @abstractmethod
