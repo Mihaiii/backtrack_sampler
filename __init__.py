9a638999d6bc8f16aaa4195d299d34f70ab370d8
Mihai <mihai.chirculescu@yahoo.com>
2024-10-09 21:27:48 +0300

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .backtrack_sampler import BacktrackSampler
+from .strategy.antislop_strategy import AntiSlopStrategy
+from .strategy.creative_writing_strategy import CreativeWritingStrategy
+from .provider.transformers_provider import TransformersProvider
+from .provider.llamacpp_provider import LlamacppProvider
+__all__ = ['BacktrackSampler', 'AntiSlopStrategy', 'CreativeWritingStrategy', 'TransformersProvider', 'LlamacppProvider']
---
+++ provider/llamacpp_provider.py
@@ -0,0 +1,22 @@
+from .backtrack_sampler_provider import BacktrackSamplerProvider
+import torch
+from typing import List
+
+class LlamacppProvider(BacktrackSamplerProvider):
+    def encode(self, text: str, add_special_tokens: bool=True) -> List[int]:
+        pass
+
+    def decode(self, tokens: List[int]) -> str:
+        pass
+
+    def generate(self, input_ids: torch.Tensor) -> torch.Tensor:
+        pass
+
+    def get_eos_token_id(self) -> int:
+        pass
+
+    def crop_cache(self, idx: int) -> None:
+        pass
+
+    def on_finish(self) -> None:
+        pass
