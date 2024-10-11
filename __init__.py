f7b783af1b28c5a6ec0eaa4b3470c78500ead06f
Mihai Chirculescu <mihai.chirculescu@yahoo.com>
2024-10-11 20:14:02 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .backtrack_sampler import BacktrackSampler
 from .strategy.antislop_strategy import AntiSlopStrategy
 from .strategy.creative_writing_strategy import CreativeWritingStrategy
-__all__ = ['BacktrackSampler', 'AntiSlopStrategy',
+__all__ = ['BacktrackSampler',
+           'AntiSlopStrategy',
            'CreativeWritingStrategy']
