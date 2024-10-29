03c01cec3d7331031ab7f7889394de5e460f5fe2
Mihai Chirculescu <mihai.chirculescu@yahoo.com>
2024-10-29 12:02:43 +0200

    Update init for adaptive temp strategy

--- __init__.py
+++ __init__.py
@@ -3,8 +3,10 @@ from .strategy.antislop_strategy import AntiSlopStrategy
 from .strategy.creative_writing_strategy import CreativeWritingStrategy
 from .strategy.debug_strategy import DebugStrategy
 from .strategy.human_guidance_strategy import HumanGuidanceStrategy
+from .strategy.adaptive_temperature_strategy import AdaptiveTemperatureStrategy
 __all__ = ['BacktrackSampler',
            'AntiSlopStrategy',
            'CreativeWritingStrategy',
            'DebugStrategy',
-           'HumanGuidanceStrategy']
+           'HumanGuidanceStrategy',
+           'AdaptiveTemperatureStrategy']
