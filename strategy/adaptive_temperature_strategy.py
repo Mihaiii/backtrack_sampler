cbd137bb7eb8b4526e5bfcb304899d4f4860fb80
Mihai Chirculescu <mihai.chirculescu@yahoo.com>
2024-10-29 12:01:44 +0200

    Add sources for adaptive temp strategy

--- strategy/adaptive_temperature_strategy.py
+++ strategy/adaptive_temperature_strategy.py
@@ -1,9 +1,9 @@
 import torch
 from typing import List
 from .base_strategy import BaseStrategy
-from ..provider.base_provider import BaseProvider
 
-
+# https://colab.research.google.com/drive/18-2Z4TMua-nwgCpIZo0lsKL6RDxH5Bvo?usp=sharing#scrollTo=dGj0lDA8wTlC
+# https://github.com/Pleias/Quest-Best-Tokens
 class AdaptiveTemperatureStrategy(BaseStrategy):
     def __init__(
         self,
